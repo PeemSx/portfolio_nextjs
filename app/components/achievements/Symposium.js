b8eb664159687dff8934d31ec80aacba00de5974
PeemSx <peemp5802@gmail.com>
2024-01-14 13:36:57 +0700

    edit Cert size

--- app/components/achievements/Symposium.js
+++ app/components/achievements/Symposium.js
@@ -5,7 +5,7 @@ const Symposium = () => {
     <div className='p-8 m-8 flex flex-col justify-center items-center space-y-4 shadow-2xl' style={{minHeight: "40vh",backgroundColor: "#474747" }}>
       <h1 className='text-center text-lg'>PCSHS Symposium</h1>
      
-        <img src='images/Symposium.png' alt='posn' className='w-1/2 mx-auto'></img>
+        <img src='images/Symposium.png' alt='posn' className='w-2/3 mx-auto'></img>
         <h2 className='text-balance text-center'>
             I was quanlified to be an English oral presenter to present my game project.         
         </h2>
